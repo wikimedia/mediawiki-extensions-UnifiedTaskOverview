6feca24c2e68cbed59880746c684812daf1415dd
hslater <hamish.slater@hallowelt.com>
2025-03-13 17:30:40 +1300

    eslint
    ERM20742
    
    [REL1_43, master]
    
    Change-Id: I6bedf8cc22f1fdab2de851edb08ade9dc0e95c6e

---
+++ Gruntfile.js
@@ -0,0 +1,23 @@
+'use strict';
+
+module.exports = function ( grunt ) {
+	const conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true,
+				fix: grunt.option( 'fix' )
+			},
+			all: '.'
+		},
+		banana: conf.MessagesDirs
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'default', 'test' );
+};
