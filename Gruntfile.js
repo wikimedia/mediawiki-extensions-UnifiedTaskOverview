56b17e4f67500c42e5e131433434feb31c35162b
Hamish <hamish.slater@hallowelt.com>
2026-07-14 17:03:18 +0700

    eslint
    Change-Id: I203c30b1515ec447016d4b9e266a742b9532ac4c

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,7 +5,6 @@ module.exports = function ( grunt ) {
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
-	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		eslint: {
