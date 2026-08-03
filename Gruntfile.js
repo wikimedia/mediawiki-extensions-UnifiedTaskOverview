397d3be27bb736f64339ffed7fc37247fed8f101
Hamish <hamish.slater@hallowelt.com>
2026-08-03 23:52:19 +0700

    eslint

--- Gruntfile.js
+++ Gruntfile.js
@@ -5,6 +5,7 @@ module.exports = function ( grunt ) {
 
 	grunt.loadNpmTasks( 'grunt-banana-checker' );
 	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
 
 	grunt.initConfig( {
 		eslint: {
@@ -14,9 +15,19 @@ module.exports = function ( grunt ) {
 			},
 			all: '.'
 		},
+		stylelint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
 		banana: conf.MessagesDirs
 	} );
 
-	grunt.registerTask( 'test', [ 'eslint', 'banana' ] );
+	grunt.registerTask( 'test', [ 'eslint', 'stylelint', 'banana' ] );
 	grunt.registerTask( 'default', 'test' );
 };
